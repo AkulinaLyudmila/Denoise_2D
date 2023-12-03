bcc27c294b29a91ec7d0a9abb140173c137eeae4
Lewda <the_toude@mail.ru>
2023-12-04 00:37:44 +0300

    Changing_graph_location_and_default_values

--- gui_logic.py
+++ gui_logic.py
@@ -41,9 +41,39 @@ class GuiProgram(Ui_Dialog):
         )
 
         # ДЕЙСТВИЯ ПРИ ВКЛЮЧЕНИИ
-        self.draw()
+        self.pushButton_signal.clicked.connect(self.signal)
 
     def draw(self):
         x = 10 * np.random.rand(5, 3)
 
         drawer.graph_2d_gray(self.graph_1, x)
+
+    def signal(self):
+        Amplitudes = [int(self.lineEdit_dome_amplitude_1.text()),
+                      int(self.lineEdit_dome_amplitude_2.text()),
+                      int(self.lineEdit_dome_amplitude_3.text())]
+        sigma = [int(self.lineEdit_dome_sigma_1.text()),
+                 int(self.lineEdit_dome_sigma_2.text()),
+                 int(self.lineEdit_dome_sigma_3.text())]
+        x0 = [int(self.lineEdit_dome_bias_x_1.text()),
+              int(self.lineEdit_dome_bias_x_2.text()),
+              int(self.lineEdit_dome_bias_x_3.text())]
+        y0 = [int(self.lineEdit_dome_bias_y_1.text()),
+              int(self.lineEdit_dome_bias_y_2.text()),
+              int(self.lineEdit_dome_bias_y_3.text())]
+        N = 512
+        M = 512
+
+        x = np.arange(0,N)
+        y = np.arange(0,M)
+        xx, yy = np.meshgrid(x, y)
+
+        z1 = Amplitudes[0] * np.exp(-((xx - x0[0]) ** 2 + (yy - y0[0]) ** 2) / (2 * sigma[0] ** 2))
+        z2 = Amplitudes[1] * np.exp(-((xx - x0[1]) ** 2 + (yy - y0[1]) ** 2) / (2 * sigma[1] ** 2))
+        z3 = Amplitudes[2] * np.exp(-((xx - x0[2]) ** 2 + (yy - y0[2]) ** 2) / (2 * sigma[2] ** 2))
+
+        z = z1 + z2 + z3
+
+        drawer.graph_2d_gray(self.graph_1, z)
+
+        return z
